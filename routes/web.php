a17490814416b680d4008fab36d67d3a18493467
Fedesin <fedesimone31@gmail.com>
2026-05-18 16:32:10 -0300

    clase 5

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,22 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return 'Ruta home';
+    return view('home');
 });
 
 Route::get('blog', function () {
-    return 'Listado de publicaciones';
+    // consulta a la base de datos
+    $posts = [
+        ['id'=>1, 'title'=>'PHP', 'slug'=>'php'],
+        ['id'=>2, 'title'=>'Laravel', 'slug'=>'laravel'],
+    ];
+    return view('blog', ['posts' => $posts]);
 });
 
 Route::get('blog/{slug}', function ($slug) {
     // return 'Publicación: ' . $slug;
-    return 'Publicación: ' . $slug;
-});
-
-Route::get('buscar', function (Request $request) {
-    return $request->all();
+    $post = $slug;
+    return view('post', ['post' => $post]);
 });
