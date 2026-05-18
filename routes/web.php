e943d5d3d02c2c116578f0d7f0ac758431d00338
Fedesin <fedesimone31@gmail.com>
2026-05-18 16:06:12 -0300

    clase 4

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,21 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'Ruta home';
+});
+
+Route::get('blog', function () {
+    return 'Listado de publicaciones';
+});
+
+Route::get('blog/{slug}', function ($slug) {
+    // return 'Publicación: ' . $slug;
+    return 'Publicación: ' . $slug;
+});
+
+Route::get('buscar', function (Request $request) {
+    return $request->all();
 });
