6824ed5bdaf4c5f4b121b01184dd5ed69e8430a0
Fedesin <fedesimone31@gmail.com>
2026-05-20 16:18:33 -0300

    clase 7 Controladores

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,19 @@
 <?php
 
+use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
+/*
+Route::get('/', [PageController::class, 'home'])->name('home');
 
-Route::get('/', function () {
-    return view('home');
-})->name('home');
+Route::get('blog', [PageController::class, 'blog'])->name('blog');
 
-Route::get('blog', function () {
-    // consulta a la base de datos
-    $posts = [
-        ['id'=>1, 'title'=>'PHP', 'slug'=>'php'],
-        ['id'=>2, 'title'=>'Laravel', 'slug'=>'laravel'],
-    ];
-    return view('blog', ['posts' => $posts]);
-})->name('blog');
+Route::get('blog/{slug}', [PageController::class, 'post'])->name('post');;
+*/
 
-Route::get('blog/{slug}', function ($slug) {
-    // return 'Publicación: ' . $slug;
-    $post = $slug;
-    return view('post', ['post' => $post]);
-})->name('post');;
+Route::controller(PageController::class)->group(function () {
+
+    Route::get('/', 'home')->name('home');
+    Route::get('blog', 'blog')->name('blog');
+    Route::get('blog/{slug}', 'post')->name('post');
+
+});
