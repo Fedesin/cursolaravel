6a6222e228853d4888e8e81bead7f1c240d2d14a
Fedesin <fedesimone31@gmail.com>
2026-05-27 16:43:16 -0300

    clase 22

--- resources/views/posts/_form.blade.php
+++ resources/views/posts/_form.blade.php
@@ -1,13 +1,17 @@
 @csrf
 
 <label class="uppercase text-black-700 text-xs">Título</label>
-<input type="text" name="title" class="rounded border-gray-200 w-full mb-4 bg-gray-800" value="{{ $post->title  }}">
+<span>@error('title') <span class="text-red-500 text-xs">*{{ $message }}</span> @enderror</span>
+<input type="text" name="title" class="rounded border-gray-200 w-full mb-4 bg-gray-800"
+value="{{ old('title', $post->title) }}"
+>
 
 <label class="uppercase text-black-700 text-xs">Contenido</label>
+<span>@error('body') <span class="text-red-500 text-xs">*{{ $message }}</span> @enderror</span>
 <textarea name="body" rows="5" class="rounded border-gray-200 w-full mb-4 bg-gray-800">{{ $post->body  }}</textarea>
 
 <div class="flex justify-between">
-    <a href="{{ route('posts.index') }}" class="text-indigo-600">Volver></a>
+    <a href="{{ route('posts.index') }}" class="text-indigo-600">Volver</a>
 
-    <input type="submit" value="Enviar" class="bg-indigo-800 text-white  px-4 py-2 rounded">>
+    <input type="submit" value="Enviar" class="bg-indigo-800 text-white  px-4 py-2 rounded">
 </div>
