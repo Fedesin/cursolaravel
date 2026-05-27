f9737d948b04c93d6dc20ad21ac6815d2bf56ae4
Fedesin <fedesimone31@gmail.com>
2026-05-27 16:13:31 -0300

    clase 19 y 20

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -10,6 +10,8 @@ class Post extends Model
     /** @use HasFactory<\Database\Factories\PostFactory> */
     use HasFactory;
 
+    protected $fillable = ['title','slug','body'];
+
     public function user()
     {
         return $this->belongsTo(User::class);
--- resources/views/posts/_form.blade.php
+++ resources/views/posts/_form.blade.php
@@ -1,1 +1,13 @@
-Formulario
+@csrf
+
+<label class="uppercase text-black-700 text-xs">Título</label>
+<input type="text" name="title" class="rounded border-gray-200 w-full mb-4 bg-gray-800" value="{{ $post->title  }}">
+
+<label class="uppercase text-black-700 text-xs">Contenido</label>
+<textarea name="body" rows="5" class="rounded border-gray-200 w-full mb-4 bg-gray-800">{{ $post->body  }}</textarea>
+
+<div class="flex justify-between">
+    <a href="{{ route('posts.index') }}" class="text-indigo-600">Volver></a>
+
+    <input type="submit" value="Enviar" class="bg-indigo-800 text-white  px-4 py-2 rounded">>
+</div>
