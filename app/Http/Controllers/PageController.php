5c59cb7532647173e7423d7a63230b24c0522f9d
Fedesin <fedesimone31@gmail.com>
2026-06-02 16:10:54 -0300

    clase 26

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -9,17 +9,8 @@ class PageController extends Controller
 {
     public function home()
     {
-        return view('home');
-    }
-
-    public function blog()
-    {
-        // consulta a la base de datos
-
-        //quiero traer a todos y paginarlos
         $posts = Post::paginate();
-
-        return view('blog', ['posts' => $posts]);
+        return view('home', ['posts' => $posts]);
     }
 
     public function post(Post $post)
